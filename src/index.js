271c29d302b0c5c67faddd81cbfc0470029a4235
UjW0L <bktujwol@gmail.com>
2023-07-21 10:33:43 +0545

    Version 1.1

---
+++ src/index.js
@@ -0,0 +1,58 @@
+
+/**
+ * Registers a new block provided a unique name and an object defining its behavior.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
+ */
+import { registerBlockType } from '@wordpress/blocks';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * All files containing `style` keyword are bundled together. The code used
+ * gets applied both to the front of your site and to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './style.scss';
+
+
+
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import Edit from './edit';
+import save from './save';
+import metadata from './block.json';
+
+/**
+ * Every block starts by registering a new block type definition.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
+ */
+registerBlockType( metadata.name, {
+
+	keywords : [__('Sentiment Analysis','tf-sa'),__('Machine Learning','tf-sa')],
+
+	attributes: {
+		text: { type: 'String', default: '' },
+		color: { type: 'String', default: '' },
+		sentiment: { type: 'string', default: '' },
+		textLength: { type: 'Number', default: 0 },
+	},
+	/**
+	 * @see ./edit.js
+	 */
+	edit: Edit,
+
+	/**
+	 * @see ./save.js
+	 */
+	save,
+} );
