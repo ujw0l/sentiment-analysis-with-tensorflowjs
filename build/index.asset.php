e140164899b8b21c47ddab8e867b3833ac17cfd2
ujwol <bktujwol@gmail.com>
2024-05-08 07:46:04 +0545

    Some update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '850f1ce648ff132da75b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8fee3dbf439e49901fec');
